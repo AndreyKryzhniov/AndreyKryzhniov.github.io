c095a2ea15390a6787fcf69bf4e61cabbafec1dc
AndreyKryzhniov <andreykryzhniov@gmail.com>
2019-10-14 18:36:02 +0300

    Updates

---
+++ precache-manifest.a694876673bc7ebf7edd6f4faff9c101.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d08e48db25478387260090f6083e684b",
+    "url": "/my_portfolio/index.html"
+  },
+  {
+    "revision": "57bc7e219be57cf69868",
+    "url": "/my_portfolio/static/css/main.378255c2.chunk.css"
+  },
+  {
+    "revision": "aa8e95da387675c4cced",
+    "url": "/my_portfolio/static/js/2.1ba6e471.chunk.js"
+  },
+  {
+    "revision": "57bc7e219be57cf69868",
+    "url": "/my_portfolio/static/js/main.e7b2c360.chunk.js"
+  },
+  {
+    "revision": "4fb7f76e07422f59be56",
+    "url": "/my_portfolio/static/js/runtime-main.2c439ad1.js"
+  },
+  {
+    "revision": "328dd7ac0b246d77fb9487ba207920ac",
+    "url": "/my_portfolio/static/media/Avatar.328dd7ac.jpg"
+  }
+]);
